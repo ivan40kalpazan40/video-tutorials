b1f9ccd669a1ef1ee562bc731e2999eba60f77e9
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-12 17:51:03 +0300

    hash password method in User Schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   enrolledIn: [{ type: mongoose.Types.ObjectId, ref: 'Course' }],
 });
-
+userSchema.pre('save', function (next) {
+  bcrypt.hash(this.password, 12).then((hash) => {
+    this.password = hash;
+    next();
+  });
+});
 const User = mongoose.model('User', userSchema);
 module.exports = User;
