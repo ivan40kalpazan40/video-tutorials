eb19a0ce2d78c23c2662aa03db29e74f7968a4e2
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-12 17:00:50 +0300

    connect to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const path = require('path');
 
@@ -7,7 +8,17 @@ require('./config/handlebars')(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
 
-app.listen(
-  3000,
-  console.log.bind(console, 'This server works at port 3000 ....')
-);
+mongoose
+  .connect('mongodb://localhost:27017/tutorials')
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .then(() => {
+    app.listen(
+      3000,
+      console.log.bind(console, 'This server works at port 3000 ....')
+    );
+  })
+  .catch((err) => {
+    console.log('CONNECTION ERROR :: ' + err.message);
+  });
