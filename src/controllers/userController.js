b21995b42d8ac26ef9715f2dc831934d0895fef6
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-12 16:10:03 +0300

    work on user controller and views

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const userHomePage = (req, res) => {
+    res.render('user/user-home')
+}
+
 const renderRegister = (req, res) => {
   res.render('user/register');
 };
+const renderLogin = (req, res) => {
+  res.render('user/login');
+};
 
 router.get('/register', renderRegister);
+router.get('/login', renderLogin);
+router.get('/', userHomePage)
 
 module.exports = router;
