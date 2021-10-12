d70dc8860821c9d8b4fb3b50bffe16cf3028a36b
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-12 15:04:58 +0300

    handlebars settings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
+const handlebars = require('express-handlebars');
 
 const app = express();
+app.set('views', path.resolve('./src/views'));
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.send('test');
+  res.render('index');
 });
 
 app.listen(
