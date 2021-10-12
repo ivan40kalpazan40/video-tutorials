1846a1f972b6cc47ddc04f6b5ea415593a8c9403
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-12 15:35:33 +0300

    handle non existing routes with redirect to /

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const homeController = require('./controllers/homeController');
 const router = express.Router();
 
+const pageNotFound = (req, res) => {
+  res.status(404).redirect(`/`);
+};
 router.use(homeController);
+router.use('*', pageNotFound);
 
 module.exports = router;
