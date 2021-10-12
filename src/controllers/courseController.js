77424e57f2a053dcc3b06e2fe9a5e179260a3848
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-12 16:38:10 +0300

    render details page for single course

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -5,6 +5,10 @@ const renderCreate = (req, res) => {
   res.render('course/create');
 };
 
+const renderDetails = (req, res) => {
+  res.render('course/details');
+};
 router.use('/create', renderCreate);
+router.use('/details', renderDetails);
 
 module.exports = router;
