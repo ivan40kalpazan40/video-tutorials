ab32cde6f30ce81ec9e42e28e8ed3e367d279145
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-13 12:46:56 +0300

    display courses on loggedOut home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const courseServices = require('../services/courseServices');
 const { homeLogged } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-const renderHome = (req, res) => {
-  res.render('index', { user: req.user });
+const renderHome = async (req, res) => {
+  const courses = await courseServices.getAll();
+  res.render('index', { courses, user: req.user });
 };
 
 router.get('/', homeLogged, renderHome);
